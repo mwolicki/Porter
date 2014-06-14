82ed067252237461f997b93450da545f0a41307e
mwolicki <marcin.wolicki@gmail.com>
2014-06-14 20:12:11 +0100

    created new namespaces: ViewModel & Command

---
+++ src/PorterApp/Command/OpenObjectViewCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PorterApp
+{
+	internal class OpenObjectViewCommand : ICommand
+	{
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object parameter)
+		{
+			var objectDetails = new ObjectDetails
+			{
+				DataContext = new ObjectDetailsViewModel
+				{
+					ObjectRef = ((MainViewModel) parameter).SelectedObject.ObjectRef
+				}
+			};
+			objectDetails.Show();
+		}
+
+		public event EventHandler CanExecuteChanged;
+	}
+}
